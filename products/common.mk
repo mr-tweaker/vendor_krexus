cbfaba11687ec730fd659233197bfe1ae449ea63
George G <kreach3r@users.noreply.github.com>
2016-04-21 19:09:56 +0300

    Moar vendor refactoring #3
    This still needs more work.

--- products/common.mk
+++ products/common.mk
@@ -5,17 +5,18 @@ PRODUCT_DEVICE := generic
 
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
-    ro.com.google.clientidbase=android-google \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.config.alarm_alert=Oxygen.ogg \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false \
     ro.setupwizard.network_required=false \
     ro.setupwizard.gservices_delay=-1 \
     ro.adb.secure=1 \
-    ro.krexus.version=krexus_mm-$(shell date +"%y%m%d")-$(TARGET_DEVICE)
+    ro.krexus.version=krexus_mm-caf-$(shell date +"%y%m%d")-$(TARGET_DEVICE)
 
 # Thank you, please drive thru!
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
@@ -23,9 +24,15 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/krexus/overlay/common
 
-# Bootanimation
+# Bootanimation (add if it exists)
+ifneq ($(wildcard vendor/krexus/prebuilt/bootanimation/$(TARGET_DEVICE).zip),)
 PRODUCT_COPY_FILES += \
-    vendor/krexus/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
+    vendor/krexus/prebuilt/bootanimation/$(TARGET_DEVICE).zip:system/media/bootanimation.zip
+else
+# otherwise use the default one
+PRODUCT_COPY_FILES += \
+    vendor/krexus/prebuilt/bootanimation/default.zip:system/media/bootanimation.zip
+endif
 
 # Extra Packages
 PRODUCT_PACKAGES += \
@@ -34,6 +41,15 @@ PRODUCT_PACKAGES += \
     LiveWallpapersPicker \
     Stk \
     WallpaperPicker
+
+# Latin IME lib (based on ARM architecture)
+ifneq ($(filter arm,$(TARGET_ARCH)),)
+PRODUCT_COPY_FILES += \
+    vendor/krexus/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/krexus/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so
+endif
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
@@ -101,26 +117,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/krexus/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
 
-# Proprietary latinime libs needed for Keyboard swyping
-ifneq ($(filter arm64,$(TARGET_ARCH)),)
-PRODUCT_COPY_FILES += \
-    vendor/krexus/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
-else
-PRODUCT_COPY_FILES += \
-    vendor/krexus/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so
-endif
-
 # by default, do not update the recovery with system updates
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 
 # init.d support
 PRODUCT_COPY_FILES += \
     vendor/krexus/prebuilt/common/bin/sysinit:system/bin/sysinit \
-    vendor/krexus/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/krexus/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
-
-# Init file
-PRODUCT_COPY_FILES += \
-    vendor/krexus/prebuilt/common/etc/init.local.rc:root/init.local.rc
+    vendor/krexus/prebuilt/common/root/init.local.rc:root/init.local.rc \
+    vendor/krexus/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner
 
 $(call inherit-product-if-exists, vendor/extra/product.mk)
--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,7 +1,5 @@
-#
-# This policy configuration will be used by all products that
-# inherit from CM
-#
+# This policy configuration will be used by all products
+# Requires commit on platform/build
 
 BOARD_SEPOLICY_DIRS += \
-    vendor/aosp/sepolicy
+    vendor/krexus/sepolicy
