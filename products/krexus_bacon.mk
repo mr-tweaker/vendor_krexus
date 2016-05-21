3cdc438684f0aef1d90d802d91e4df9d07d410f8
George G <kreach3r@users.noreply.github.com>
2016-05-21 13:45:57 +0300

    Add krexus_bacon.mk

---
+++ products/krexus_bacon.mk
@@ -0,0 +1,23 @@
+# Inherit device configuration.
+$(call inherit-product, device/oneplus/bacon/bacon.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/krexus/products/common.mk)
+
+# Inherit vendor specific product files.
+$(call inherit-product, vendor/krexus/products/vendorless.mk)
+
+# Inherit maintainer information (if exists).
+$(call inherit-product-if-exists, device/oneplus/bacon/krexus_maintainer.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := krexus_bacon
+PRODUCT_BRAND := OnePlus
+PRODUCT_DEVICE := bacon
+PRODUCT_MODEL := A0001
+PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bacon
