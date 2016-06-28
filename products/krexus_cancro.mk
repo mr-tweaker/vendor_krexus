4946f8e48bb1879f7d5dbabf90a3b0dce646af43
mr-tweaker <aniket.lamba@gmail.com>
2016-06-28 19:24:52 +0530

    Override the PRODUCT_NAME

--- products/krexus_cancro.mk
+++ products/krexus_cancro.mk
@@ -10,10 +10,14 @@ $(call inherit-product, vendor/krexus/products/vendorless.mk)
 # Inherit maintainer information (if exists).
 $(call inherit-product-if-exists, device/xiaomi/cancro/krexus_maintainer.mk)
 
-PRODUCT_NAME := krexus_cancro
+# Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := cancro
+PRODUCT_NAME := krexus_cancro
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := MI Cancro
+PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=cancro
